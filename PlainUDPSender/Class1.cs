fa25477576040133391fb970997319694647d4cc
natsachaj <natascha_jakobsen@hotmail.com>
2017-10-10 12:46:06 +0200

    klasser oprettet

---
+++ PlainUDPSender/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlainUDPSender
+{
+    public class Class1
+    {
+    }
+}
